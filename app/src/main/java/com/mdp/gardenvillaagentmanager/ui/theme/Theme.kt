cf13258421770bc602df247c50e2cbcbc55b9840
mohammadmdp <mohammadmdp@gmail.com>
2024-01-09 13:41:47 +0330

    Added Dark Theme Colors

--- app/src/main/java/com/mdp/gardenvillaagentmanager/ui/theme/Theme.kt
+++ app/src/main/java/com/mdp/gardenvillaagentmanager/ui/theme/Theme.kt
@@ -18,17 +18,20 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
         primary = Purple80,
         secondary = PurpleGrey80,
-        tertiary = Pink80
+        tertiary = Pink80,
+        background = DarkTheme1,
+        surface = DarkTheme1,
 )
 
 private val LightColorScheme = lightColorScheme(
         primary = Purple40,
         secondary = PurpleGrey40,
-        tertiary = Pink40
+        tertiary = Pink40,
+        background = LightTheme1,
+        surface = LightTheme1,
 
         /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
